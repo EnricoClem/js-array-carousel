344c8ee59a3b05a2e76c15eaf74db828db352ae2
EnricoClem <enrico.clem94@gmail.com>
2024-03-08 15:33:16 +0100

    ADD - html and css structure and js instructions

---
+++ app.js
@@ -0,0 +1,10 @@
+// Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+
+// Recuperare le immagini come percorsi di recupero delle stesse e metterle in un array
+// Recuperare l'elemento items-container
+// Collocare in items-container le immaginiindicate nell'array
+// Usare un ciclo per creare div nel dom facendo riferimento alla lista contenuta nell'array
+// Applicare la classe item--active alla prima immagine
+// Rendere funzionante la classe next spostando al click la classe item--active al item sucessivo
+// Applicare lo stesso ragionamento a prev modificando l'andamento a ritroso
+// Rendere i tasti prev e next funzionanti anche ai limiti delle spostando item--active al item opposto della lista
